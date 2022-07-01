bfc780dc238a8a9a211c6099ce9a38cbea737f3c
Ian <ianmcm.89@gmail.com>
2022-07-01 04:52:14 -0400

    create user test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  username: "user1",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user2",
+  password: "password1",
+  password_confirmation: "password1"
+)
