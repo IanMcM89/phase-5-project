7cdad16b4a3bd804dc7f3b37c1614064fccb76b1
Ian <ianmcm.89@gmail.com>
2022-07-02 05:45:41 -0400

    create Header.js in styled components

---
+++ client/src/styles/Header.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Header = styled.header`
+  background-color: darkblue;
+  border-bottom: 0.75rem solid orangered;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  width: auto;
+  height: 10vh;
+  margin: 0;
+  top: 0;
+`;
+
+export default Header;
