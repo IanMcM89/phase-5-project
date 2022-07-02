113ee43cb8dbc0ac8f61b8890ddfc7ee10db1637
Ian <ianmcm.89@gmail.com>
2022-07-02 05:46:20 -0400

    create Index.js in styled components

---
+++ client/src/styles/index.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import Form from "./Form";
+import FormField from "./FormField";
+import Input from "./Input";
+import Label from "./Label";
+import Error from "./Error";
+import Button from "./Button";
+
+export { Header, Form, FormField, Input, Label, Error, Button };
