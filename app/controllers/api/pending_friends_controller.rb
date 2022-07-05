a33b9d11fa8fe010055ec19b4cae6923b9cc7b5c
Ian <ianmcm.89@gmail.com>
2022-07-05 07:36:10 -0400

    add pending_friends controller

---
+++ app/controllers/api/pending_friends_controller.rb
@@ -0,0 +1,19 @@
+class Api::PendingFriendsController < ApplicationController
+  before_action :find_pending_friend, except: :index
+
+  def index
+    render json: @current_user.pending_friends.to_json(only: [:id, :username])
+  end
+
+  def show
+    render json: @pending_friend.to_json(only: [:id, :username])
+  end
+
+  private
+
+  def find_pending_friend
+    @pending_friend = @current_user.pending_friends.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: "Pending friend not found", status: :not_found
+  end
+end
