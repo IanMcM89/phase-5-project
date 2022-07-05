61c57b3fed47decdba741cd251fe902fc642c9a0
Ian <ianmcm.89@gmail.com>
2022-07-05 07:34:56 -0400

    add validation methods for friends and requests

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,21 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  private
+
+  def befriending_self?
+    errors.add(:friend, "can't befriend self") if user == friend
+  end
+
+  def already_friends?
+    errors.add(:friend, "already added") if user.friends.include?(friend)
+  end
+
+  def already_requested?
+    errors.add(:friend, "already requested") if user.pending_friends.include?(friend)
+  end
+
+  def already_pending?
+    errors.add(:friend, "is pending") if friend && friend.pending_friends.include?(user)
+  end
 end
