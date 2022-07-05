d5001851585a87dbd578405866ca4f8272e79537
Ian <ianmcm.89@gmail.com>
2022-07-05 07:37:20 -0400

    remove unused methods and update associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,10 @@
 class User < ApplicationRecord
-  has_many :friendships, -> {where(status: true)}
+  has_many :friendships
   has_many :friends, through: :friendships, dependent: :destroy
-  has_many :friend_requests, -> {where(status: false)}, class_name: 'Friendship'
+  has_many :friend_requests, dependent: :destroy
   has_many :pending_friends, through: :friend_requests, source: :friend
 
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
-
-  def friend_request(user)
-    self.pending_friends << user
-  end
-
-  def accept(user)
-    friendships = []
-    friendships << find_friendship(self, user)
-    friendships << find_friendship(user, self)
-    friendships.each do |f|
-      f.update(status: true)
-    end
-  end
-
-  def unfriend(user)
-    friendship = find_friendship(self, user)
-    friendship.destroy_all
-  end
-
-  def find_friendship(user_a, user_b)
-    Friendship.where(user: user_a, friend: user_b)
-  end
-
-  def has_friend?
-    Friendship.exists?({friend_id: user_id, user_id: friend_id})
-  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
+
+  has_many :friends
+  has_many :pending_friends
 end
