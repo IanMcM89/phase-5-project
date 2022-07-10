1b9af2d2de8d1aa7d92646f129d07aade2e204a8
Ian <ianmcm.89@gmail.com>
2022-07-10 05:39:02 -0400

    cosmetic changes

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -5,48 +5,62 @@ import styled from "styled-components";
 const Menu = () => {
   return (
     <Wrapper>
-      <MenuIcon
-        src="/images/icons/menu.png"
-        alt="Menu"
-      />
       <Nav>
-        <Link to="/">
+        <IconWrapper1>
           <Icon
-            src="/images/icons/map.png"
-            alt="GeoMapper"
+            src="/images/icons/menu.png"
+            alt="Menu"
+            style={{ cursor: 'default' }}
           />
-        </Link>
-        <Link to="/events">
-          <Icon
-            src="/images/icons/events.png"
-            alt="Events"
-          />
-        </Link>
-        <Link to="/friends">
-          <Icon
-            src="/images/icons/friends.png"
-            alt="Friends"
-          />
-        </Link>
+        </IconWrapper1>
+        <IconWrapper2>
+          <Link to="/">
+            <Icon
+              src="/images/icons/map.png"
+              alt="GeoMapper"
+            />
+          </Link>
+        </IconWrapper2>
+        <IconWrapper2>
+          <Link to="/events">
+            <Icon
+              src="/images/icons/events.png"
+              alt="Events"
+            />
+          </Link>
+        </IconWrapper2>
+        <IconWrapper2>
+          <Link to="/friends">
+            <Icon
+              src="/images/icons/friends.png"
+              alt="Friends"
+            />
+          </Link>
+        </IconWrapper2>
       </Nav>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-  background-color: rgb(75, 86, 105);
+  background-color: rgb(69, 73, 86);
   display: flex;
   flex-direction: column;
   height: 100%;
-  width: 5%;
+  width: 4%;
   margin: 0;
 `;
 
-const MenuIcon = styled.img`
+const IconWrapper1 = styled.div`
+  background-color: rgb(186, 43, 43);
+  border: 1px solid gray;
   display: flex;
-  width: 60%;
-  margin: 5% auto;
-  cursor: pointer;
+  height: fit-content;
+  width: 100%;
+`;
+
+const IconWrapper2 = styled(IconWrapper1)`
+  background-color: rgb(45, 50, 61);
 `;
 
 const Nav = styled.nav`
@@ -60,7 +74,7 @@ const Nav = styled.nav`
 const Icon = styled.img`
   display: flex;
   width: 70%;
-  margin: 5% auto;
+  margin: 10% auto;
   cursor: pointer;
 `;
 
