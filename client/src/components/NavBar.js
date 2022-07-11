b6eca77e8deb5e8f79208f7e72d59cf9ac287353
Ian <ianmcm.89@gmail.com>
2022-07-10 20:00:20 -0400

    import Link from react and add css changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Header, Button } from "../styles";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-function NavBar({ user, setUser }) {
+const NavBar = ({ setUser }) => {
   const handleLogout = () => {
     fetch("/api/logout", {
       method: "DELETE"
@@ -16,63 +17,56 @@ function NavBar({ user, setUser }) {
   return (
     <Header>
       <LogoWrapper>
-        <Logo
-          src="/images/rosa-pin.png"
-          alt="Rosa-Pin Logo" 
-        />
-        <Title>GeoPlanner</Title>
+        <Logo src="/images/pin.png" alt="Pin Logo" />
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <Title>GeoPlanner</Title>
+        </Link>
       </LogoWrapper>
       <Nav>
-        <Icon 
-          src="/images/icons/profile.png"
-          alt="Profile"
-        />
-        <Icon 
-          src="/images/icons/bell-fill.png"
-          alt="Notifications"
-          style={{ marginRight: '1%'}}
-        />
-        <Button variant="red" onClick={handleLogout}>
+        <Icon src="/images/icons/profile.png" alt="Profile" />
+        <Icon src="/images/icons/bell-fill.png" alt="Notifications" />
+        <Button variant="tr-red" onClick={handleLogout}>
           Logout
         </Button>
       </Nav>
     </Header>
   )
-}
+};
 
-const LogoWrapper = styled.div`
+const commonStyles = css`
   display: flex;
   flex-direction: row;
+  align-items: center;
+`;
+
+const LogoWrapper = styled.div`
+  ${commonStyles}
   height: 100%;
   margin-left: 5px;
 `;
 
 const Logo = styled.img`
-  display: flex;
   height: 50%;
   margin: auto 5%;
 `;
 
 const Title = styled.h1`
   color: white;
-  display: flex;
   font-size: 2rem;
-  margin: auto 0;
+  margin: auto;
 `;
 
 const Nav = styled.nav`
-  display: flex;
-  flex-direction: row;
+  ${commonStyles}
   justify-content: right;
-  width: 49%;
   height: 100%;
   margin: 0 1% 0 auto;
 `;
 
 const Icon = styled.img`
-  display: flex;
   height: 60%;
   margin: auto 0;
+  cursor: pointer;
 `;
 
 export default NavBar;
