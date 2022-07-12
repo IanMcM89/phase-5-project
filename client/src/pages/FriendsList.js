6d7d812cf3ba3ca1c774896e00ab2969f05f656c
Ian <ianmcm.89@gmail.com>
2022-07-11 20:12:08 -0400

    add wrapper styled component

--- client/src/pages/FriendsList.js
+++ client/src/pages/FriendsList.js
@@ -3,9 +3,15 @@ import styled from "styled-components";
 
 const FriendsList = () => {
   return (
-    <>
-    </>
+    <Wrapper>
+    </Wrapper>
   )
 };
+
+const Wrapper = styled.div`
+  width: 95vw;
+  height: 90vh;
+  overflow: hidden;
+`;
 
 export default FriendsList;
