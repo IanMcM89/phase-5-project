eab80a92b980907183eaa550062ecbe3f891c7a7
Ian <ianmcm.89@gmail.com>
2022-07-11 20:12:27 -0400

    add search query functionality

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, StandaloneSearchBox, Marker } from "@react-google-maps/api";
 import styled from "styled-components";
 
-const mapStyles = {
-  width: '100%',
-  height: '100%',
-};
+const libraries = ["places"];
 
-function Map() {
-  const [map, setMap] = React.useState(null);
-  const [currentLoc, setCurrentLoc] = React.useState({
-    lat: 0,
-    lng: 0,
-  });
-
-  console.log(currentLoc)
-
-  const onLoad = React.useCallback(function callback(map) {
-    setMap(map);
-
+const Map = () => {
+  const [map, setMap] = useState(null);
+  const [coordsResult, setCoordsResult] = useState([]);
+  const [currentLoc, setCurrentLoc] = useState({ lat: 0, lng: 0 });
+  const [searchValue, setSearchValue] = useState('');
+  const [zoom, setZoom] = useState(12);
+  
+  // Fetch current user's geolocation using Google Maps API:
+  const onLoad = (map) => { setMap(map);
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         setCurrentLoc({
@@ -27,26 +21,80 @@ function Map() {
         });
       });
     }
-  }, []);
+  };
+  
+  // Add Enter key event listener to searchbox:
+  const input = document.getElementById("searchbox");
+  if (input) input.addEventListener("keyup", ({key}) => {
+    if (key === 'Enter') {
+      searchPlaces();
+    }
+  })
 
-  const handleCenterChange = () => {
-    if (map && !map) return;
-    const newPos = map.getCenter().toJSON();
-    setCurrentLoc(newPos);
+  // Fetch places request using Google Places textsearch query:
+  const searchPlaces = () => {
+    let service = new window.google.maps.places.PlacesService(map);
+    let request = {
+      query: searchValue,
+      fields: ["name", "geometry"]
+    };
+
+    if (searchValue) service.findPlaceFromQuery(request, (results, status) => {
+      let loc = results[0].geometry.location;
+
+      console.log(results)
+
+      if (status) {
+        setCoordsResult(results);
+        setZoom(14);
+        setCurrentLoc({
+          lat: loc.lat(),
+          lng: loc.lng()
+        });
+      };
+    });
+  };
+
+  // Set currentLoc state to new coords after map drag:
+  const changeCenterPos = () => {
+    if (map && !map) setCurrentLoc(map.getCenter().toJSON());
   }
 
   return (
     <Wrapper>
-      <LoadScript googleMapsApiKey={`${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}>
+      <LoadScript
+        googleMapsApiKey={`${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
+        libraries={libraries}
+      >
         <GoogleMap
           mapContainerStyle={mapStyles}
-          center={{ lat: currentLoc.lat, lng: currentLoc.lng }}
-          onDragEnd={handleCenterChange}
-          zoom={12}
+          center={{
+            lat: currentLoc.lat,
+            lng: currentLoc.lng
+          }}
+          onDragEnd={changeCenterPos}
+          zoom={zoom}
           onLoad={onLoad}
         >
-          {/* Child components, such as markers, info windows, etc. */}
-          <></>
+          <StandaloneSearchBox
+            map={map}
+          >
+            <input
+              id="searchbox"
+              type="text"
+              placeholder="Search Places"
+              style={searchStyles}
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+          </StandaloneSearchBox>
+          {coordsResult !== [] &&
+            coordsResult.map((result, i) => <Marker 
+              key={i} 
+              position={result.geometry.location} 
+              onClick={() => console.log(result)}
+            />)
+          }
         </GoogleMap>
       </LoadScript>
     </Wrapper>
@@ -58,5 +106,27 @@ const Wrapper = styled.div`
   height: 90vh;
   overflow: hidden;
 `;
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
+
+const searchStyles = {
+  boxSizing: 'border-box',
+  border: '1px solid transparent',
+  width: '240px',
+  height: '32px',
+  padding: '0 12px',
+  borderRadius: '3px',
+  boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
+  fontSize: '14px',
+  outline: 'none',
+  textOverflow: 'ellipses',
+  position: 'absolute',
+  left: '50%',
+  marginLeft: '-120px',
+  marginTop: '1%'
+};
 
 export default React.memo(Map);
