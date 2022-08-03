c4f6ef8eadbcd3b1ccf85b6531fbf7de29c7abc1
Ian <ianmcm.89@gmail.com>
2022-08-03 18:32:29 -0400

    add more users, friendships and requests to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,163 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# User Seeds
+
 User.create(
-  username: "user1",
-  password: "password1",
-  password_confirmation: "password1"
+  username: "ianmcm89",
+  password: "password",
+  password_confirmation: "password"
 )
 
 User.create(
   username: "user2",
-  password: "password2",
-  password_confirmation: "password2"
+  password: "password",
+  password_confirmation: "password"
 )
 
 User.create(
   username: "user3",
-  password: "password3",
-  password_confirmation: "password3"
+  password: "password",
+  password_confirmation: "password"
 )
+
 User.create(
   username: "user4",
-  password: "password4",
-  password_confirmation: "password4"
+  password: "password",
+  password_confirmation: "password"
 )
+
 User.create(
   username: "user5",
-  password: "password5",
-  password_confirmation: "password5"
+  password: "password",
+  password_confirmation: "password"
 )
+
+User.create(
+  username: "user6",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user7",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user8",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user9",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user10",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user11",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user12",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user13",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user14",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user15",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user16",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user17",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user18",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user19",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  username: "user20",
+  password: "password",
+  password_confirmation: "password"
+)
+
+# # Friendship Seeds
+
+# Friendship.create(
+#   user_id: 1,
+#   friend_id: 2
+# )
+
+# Friendship.create(
+#   user_id: 1,
+#   friend_id: 3
+# )
+
+# Friendship.create(
+#   user_id: 1,
+#   friend_id: 4
+# )
+
+# Friendship.create(
+#   user_id: 1,
+#   friend_id: 5
+# )
+
+# # Friend Request Seeds
+
+# FriendRequest.create(
+#   user_id: 1,
+#   friend_id: 6
+# )
+
+# FriendRequest.create(
+#   user_id: 1,
+#   friend_id: 7
+# )
+
+# FriendRequest.create(
+#   user_id: 1,
+#   friend_id: 8
+# )
