33d698ab4204eb78e9b65e44bc6462ffd2091064
Ian <ianmcm.89@gmail.com>
2022-08-12 19:57:32 -0400

    change to FriendsTab and move to /components

---
+++ client/src/components/FriendsTab.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ListFriends from "./ListFriends";
+import ListPending from "./ListPending";
+// import ListUsers from "./ListUsers";
+import styled, { css } from "styled-components";
+
+const FriendsTab = ({ user }) => {
+
+return (
+  <Wrapper>
+    <ListFriends />
+    <ListPending />
+    {/* <ListUsers user={user}/> */}
+  </Wrapper>
+)
+};
+
+const commonStyles = css`
+  display: flex;
+  width: 50%;
+  height: 100%;
+`;
+
+const Wrapper = styled.div`
+  ${commonStyles}
+  flex-direction: column;
+  background-color: rgb(32, 36, 44);
+  background: rgba(20,25,35,0.5);
+  padding: 1%;
+`;
+
+export default FriendsTab;
