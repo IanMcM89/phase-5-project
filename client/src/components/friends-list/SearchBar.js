b68b4d748b5f8618e683eed71bd914ecaf0037b1
Ian <ianmcm.89@gmail.com>
2022-08-16 21:22:35 -0400

    add refresh button to searchbar

--- client/src/components/friends-list/SearchBar.js
+++ client/src/components/friends-list/SearchBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchUsers } from "../../reducers/users";
+import { showUserList, hideUserList } from "../../reducers/userList";
 import styled, { css } from "styled-components";
 
 const SearchBar = () => {
@@ -8,29 +9,40 @@ const SearchBar = () => {
   const dispatch = useDispatch();
 
   const handleSearch = (e) => {
-    const value = e.target.value;
+    e.preventDefault();
+
     const urls = [
       '/api/pending_friends',
       '/api/friends',
       '/api/users'
     ];
 
-    urls.map((url) => dispatch(fetchUsers(url, value)));
+    urls.map((url) => dispatch(fetchUsers(url, searchValue)));
 
-    return setSearchValue(e.target.value);
+    if (searchValue.length > 0) return dispatch(showUserList()); 
   };
 
+  const handleRefresh = () => {
+    dispatch(hideUserList());
+    return setSearchValue('');
+  }
+
   return (
-    <Wrapper>
-      <Icon src="./images/icons/search.png" alt="Search Icon" />
+    <Form onSubmit={handleSearch}>
       <Input
         type="text"
         name="search"
-        placeholder="Search"
+        placeholder="Search Username"
         value={searchValue}
-        onChange={(e) => handleSearch(e)}
+        onChange={(e) => setSearchValue(e.target.value)}
       />
-    </Wrapper>
+      <Button>
+        <Icon src="./images/icons/search.png" alt="Search Icon"/>
+      </Button>
+      <Button onClick={handleRefresh}>
+        <Icon src="./images/icons/refresh.png" alt="Search Icon"/>
+      </Button>
+    </Form>
   );
 }
 
@@ -40,19 +52,31 @@ const commonStyles = css`
   margin: 0;
 `;
 
-const Wrapper = styled.div`
+const Form = styled.form`
   ${commonStyles}
   height: 5%;
   margin-bottom: 2%;
 `;
 
-const Icon = styled.img`
-  background-color: rgb(186, 43, 43);
-`;
-
 const Input = styled.input`
   ${commonStyles}
   width: 90%;
+  height: 100%;
+`;
+
+const Button = styled.button`
+  background-color: rgb(186, 43, 43);
+  border: solid 1px darkred;
+  transition: 0.3s;
+  padding: 0;
+
+  &:hover {
+    background-color: red;
+  }
+`;
+
+const Icon = styled.img`
+  ${commonStyles}
   height: 100%;
 `;
 
