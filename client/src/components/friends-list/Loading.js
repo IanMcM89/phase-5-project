e9dd66224e5f543f350a8ac8659916e25122149f
Ian <ianmcm.89@gmail.com>
2022-08-19 04:18:07 -0400

    fix list item key error

--- client/src/components/friends-list/Loading.js
+++ client/src/components/friends-list/Loading.js
@@ -2,21 +2,17 @@ import React from "react";
 import styled, { css } from "styled-components";
 
 const Loading = () => {
-  const renderDivs = () => {
-    return (
-      [...Array(2)].map((e, i) => (
-        <>
-          <LabelDiv />
-          <ListDiv />
-        </>
-      ))
-    );
-  }
-
   return (
-    <Wrapper>
-      {renderDivs()}
-    </Wrapper>
+    <>
+      {
+        [...Array(3)].map((e, i) => (
+          <Wrapper key={i}>
+            <LabelDiv />
+            <ListDiv />
+          </Wrapper>
+        ))
+      }
+    </>
   );
 }
 
@@ -25,26 +21,25 @@ const commonStyles = css`
   background-color: rgb(32, 36, 44, 0.7);
   border-radius: 6px;
   margin: 1%;
+  animation: flicker 0.6s infinite;
 `;
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
+  height: 30%;
 `;
 
 const LabelDiv = styled.div`
   ${commonStyles}
   width: 50%;
-  height: 4%;
-  animation: flicker 0.8s infinite;
+  height: 8%;
 `;
 
 const ListDiv = styled.div`
   ${commonStyles}
-  height: 26%;
-  animation: flicker 0.6s infinite;
+  height: 88%;
 `;
 
 export default Loading;
