ddfcdfd9dbdf3550a2a1d34790e58a7ec8584863
Ian <ianmcm.89@gmail.com>
2022-08-21 03:59:20 -0400

    fix quotes in /cable route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount ActionCable.server => '/cable'
+  mount ActionCable.server => "/cable"
   namespace :api do
     resources :friend_requests
     resources :pending_friends, only: [:index, :show]
