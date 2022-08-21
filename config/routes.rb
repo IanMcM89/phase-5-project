82a6fd2f0c4f46b6b6d32e46c8e44029e3ee0043
Ian <ianmcm.89@gmail.com>
2022-08-21 03:17:22 -0400

    deploy app to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
   end
   # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
