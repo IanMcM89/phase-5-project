08d15d39ce0756d3a3d6d40b5b354f3a0eb03189
Ian <ianmcm.89@gmail.com>
2022-09-08 17:32:39 -0400

    add friend request notification

---
+++ client/src/components/notifications/lists/FriendRequest.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Button } from "../../../styles";
+import styled, { css } from "styled-components";
+
+const FriendRequest = ({ request, onUpdate, onDelete }) => {
+  return (
+    <Wrapper key={request.id}>
+      <Timestamp>{request.created_at}</Timestamp>
+      <Content>
+        <Avatar
+          src={request.user.avatar ? (
+            request.user.avatar
+          ) : (
+            "/images/icons/avatar.png"
+          )}
+          alt="Avatar Image"
+        />
+        <Message>{request.user.username} sent you a friend request:</Message>
+      </Content>
+      <Buttons>
+        <Button
+          style={{ margin: "0 4%" }} 
+          variant="green"
+          onClick={() => onUpdate(request)}
+        >
+          Accept
+        </Button>
+        <Button
+          variant="red" 
+          onClick={() => onDelete(request.id)}
+        >
+          Reject
+        </Button>
+      </Buttons>
+    </Wrapper>
+  )
+}
+
+const commonStyles = css`
+  display: flex;
+  width: 100%;
+  height: fit-content;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Wrapper = styled.div`
+  ${commonStyles}
+`;
+
+const Timestamp = styled.p`
+  color: gray;
+  margin: 0 0 0 2%;
+  font-size: 0.7vw;
+`;
+
+const Content = styled.div`
+  ${commonStyles}
+  flex-direction: row;
+  align-items: center;
+  margin: auto;
+`;
+
+const Avatar = styled.img`
+  ${commonStyles}
+  width: auto;
+  height: 30px;
+  margin: 2%;
+`;
+
+const Message = styled.p`
+  ${commonStyles}
+  color: white;
+  height: auto;
+  margin: 0;
+`;
+
+const Buttons = styled.nav`
+  ${commonStyles}
+  flex-direction: row;
+  width: fit-content;
+  margin: 0 2% 2%;
+`;
+
+export default FriendRequest;
