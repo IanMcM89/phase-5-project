3fc75cd60faf28e5a5f2d2a5532fe43a15002419
Ian <ianmcm.89@gmail.com>
2022-09-11 20:17:10 -0400

    format date and time attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :user
   
   validates :title, :description, :date, :time, presence: true
+
+  def date
+    attributes['date'].strftime("%m/%d/%Y")
+  end
+
+  def time
+    attributes['time'].strftime("%H:%M")
+  end
 end
