362700bcd84a68cf3a1cd198fb5596bab838cd02
Ian <ianmcm.89@gmail.com>
2022-09-15 02:38:44 -0400

    install react static google map

--- client/src/components/events/StaticMap.js
+++ client/src/components/events/StaticMap.js
@@ -1,36 +1,34 @@
 import React from "react";
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { StaticGoogleMap, Marker } from 'react-static-google-map';
+import styled from "styled-components";
 
 const StaticMap = ({ event }) => {
-  const coords = {
-    lat: Number(event.lat),
-    lng: Number(event.lng)
-  }
+  const coords = `${event.lat},${event.lng}`
 
   return (
-    <LoadScript
-      googleMapsApiKey={`${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
-    >
-      <GoogleMap
-        options={{ gestureHandling: 'none', disableDefaultUI: true }}
-        mapContainerStyle={mapStyles}
-        clickableIcons={false}
-        center={coords}
-        zoom={12}
+    <MapWrapper>
+      <StaticGoogleMap
+        apiKey={`${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
+        maptype="terrain"
+        size="700x700"
+        zoom={14}
       >
         <Marker
-          position={coords}
+          location={coords}
+          iconURL="http://maps.google.com/mapfiles/kml/paddle/red-circle.png"
         />
-      </GoogleMap>
-    </LoadScript>
+      </StaticGoogleMap>
+    </MapWrapper>
   )
 };
 
-const mapStyles = {
-  background: 'beige',
-  width: '96%',
-  height: '96%',
-  margin: '2%',
-};
+const MapWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: beige;
+  margin: 2%;
+  overflow: hidden;
+`;
 
 export default StaticMap;
