9e3253e8dcb871582b5cf9ca363c0d51e3879180
Ian <ianmcm.89@gmail.com>
2022-09-20 05:25:24 -0400

    add loading component for notifications

---
+++ client/src/components/notifications/Loading.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Loading = () => {
+  return (
+    <>
+      {
+        [...Array(3)].map((e, i) => (
+          <Wrapper key={i}>
+            <LabelDiv />
+            <ListDiv />
+          </Wrapper>
+        ))
+      }
+    </>
+  );
+}
+
+const commonStyles = css`
+  display: flex;
+  background-color: rgb(70, 75, 85, 0.8);
+  border-radius: 6px;
+  margin: 1%;
+  animation: flicker 0.6s infinite;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 15vh;
+  padding: 2%;
+`;
+
+const LabelDiv = styled.div`
+  ${commonStyles}
+  width: 50%;
+  height: 20%;
+`;
+
+const ListDiv = styled.div`
+  ${commonStyles}
+  height: 80%;
+`;
+
+export default Loading;
